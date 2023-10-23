61e8c4e7a60165def37568d74a3e72eee1578688
Richard Gatonye <rgatonye@kpipes.co.ke>
2023-10-23 03:10:12 +0300

    commit 1st

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/gcp"
+        version = "~>4.16"
+
+    }
+  }
+  backend "gcs" {
+    bucket = "swordinc17_state_file"
+    prefix = "terraform/project_state"
+    
+  }
+  
+  required_version = ">=1.5.0"
+}
+
+provider "google" {
+    region = "europe-west2"
+    project = "kenya-402404"
+    credential = "GOOGLE_APPLICATION_CREDENTIALS"
+  
+}
